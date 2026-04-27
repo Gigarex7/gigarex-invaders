3ab034e1a47b70061a87a05410435579e31f9a50
Gigarex7 <Gigarex7@gmail.com>
2026-04-27 11:45:35 -0600

    preliminary main structure

--- main.c
+++ main.c
@@ -3,3 +3,39 @@
 Space Invaders (terminal version)
 Copyright (C) 2026 Gigarex7
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum{
+    ENEMY_NONE, ENEMY_V, ENEMY_M, ENEMY_W
+}EnemyType;
+
+typedef struct{
+    EnemyType type;
+    int points;
+    int active;
+}Enemy;
+
+typedef struct{
+    int x, y;
+}Player;
+
+typedef struct Projectile{
+    int x, y;
+    struct Projectile* next;
+}Projectile;
+
+typedef struct{
+    char name[4];
+    int score;
+}Record;
+
+// prototypes go here
+
+int main(){
+    // loop goes here
+    return 0;
+}
+
+// functions go here
