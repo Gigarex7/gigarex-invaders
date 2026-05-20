39829da9c5f2d35d164fcd79c61390aeadf6edbb
Gigarex7 <Gigarex7@gmail.com>
2026-05-19 18:25:55 -0600

    add janky movement and game loop

--- game.c
+++ game.c
@@ -4,6 +4,7 @@
 #include "game.h"
 
 void drawScreen(Player player){
+    system("cls"); // janky refresh
     for(int y=0; y<HEIGHT; y++){
         for(int x=0; x<WIDTH; x++){
             if((y==0 && x==0) || (y==HEIGHT-1 && x==0) || (y==0 && x==WIDTH-1) || (y==HEIGHT-1 && x==WIDTH-1)){
--- main.c
+++ main.c
@@ -13,6 +13,31 @@ int main(){
     // bottom middle initial position
     player.x=WIDTH/2;
     player.y=HEIGHT-2;
-    drawScreen(player);
+    // Game Loop
+    int running=1;
+    while(running){ // whatever will I do with all the time I saved by not writing it "==1"
+        drawScreen(player);
+        char input; // note that making the program not require an Enter press after each input is advanced
+        scanf(" %c", &input); // the space in " %c" ignores the newline, there's a lot of newline
+        // Left
+        if((input=='a' || input=='A') && (player.x > 1)){
+            player.x--;
+        }
+        // Right
+        if((input=='d' || input=='D') && (player.x < (WIDTH-2))){
+            player.x++;
+        }
+        // Quit
+        if(input=='q' || input=='Q'){
+            running=0;
+        }
+    }
+    // End (WIP)
+    system("cls");
+    printf("EXITING...");
     return 0;
 }
+
+/* I have this idea where "EXITING..." remains at the bottom of the
+screen while all other elements are wiped line by line before the
+program shuts down. Might become part of the cutting room floor. */
